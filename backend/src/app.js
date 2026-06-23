131886546531d402d046418fee7634a0b2de7e96
Sayyed Sania <sayyedsania87@example.com>
2026-06-23 22:44:16 +0530

    setup vercel deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,7 +28,12 @@ app.get("/test-db", async (req, res) => {
     });
   }
 });
-const port=process.env.port || 5000;
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
