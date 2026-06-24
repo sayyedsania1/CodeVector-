8445bbded97f48df42f5b95748bedbbc66d8f183
Sayyed Sania <sayyedsania87@example.com>
2026-06-24 20:27:24 +0530

    fix vercel redirect issue

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,2 @@
-const serverless = require("serverless-http");
 const app = require("../src/app");
-
-module.exports = serverless(app);
+module.exports = app;
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,47 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const productRoutes = require("./routes/productRoutes");
-const pool = require("./db/db");
 
 const app = express();
 
 app.use(cors({
-  origin: [
-    "https://code-vector-f3h3.vercel.app",
-    "https://code-vector-qx98.vercel.app"
-  ]
+  origin: "*"
 }));
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log("REQUEST HIT:", req.method, req.url);
-  next();
-});
-
-
-app.use("/products", productRoutes);
-
 app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "CodeVector Backend Running"
-  });
+  res.json({ success: true, message: "Backend working" });
 });
 
-app.get("/test-db", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.json({
-      success: true,
-      time: result.rows[0].now
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: error.message
-    });
-  }
-});
+// IMPORTANT
+app.use("/products", productRoutes);
 
 module.exports = app;
