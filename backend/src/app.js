ab731f045e05940466782c49d34d52a473507b3b
Sayyed Sania <sayyedsania87@example.com>
2026-06-24 17:04:50 +0530

    fix vercel serverless deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -39,12 +39,6 @@ app.get("/test-db", async (req, res) => {
     });
   }
 });
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
 
 module.exports = app;
